df97361c6ee75d4a29d27360283dea361ef46a0b
Fuzail palnak <fuzailpalnak@gmail.com>
2020-04-02 22:44:28 +0530

    bug for resuming lr for polydecay

--- ml/commons/scheduler.py
+++ ml/commons/scheduler.py
@@ -7,27 +7,27 @@ from ml.pt.logger import PtLogger
 
 
 class PolyLrDecay(_LRScheduler):
-    def __init__(self, decay_after_epoch, power, max_epochs, optimizer):
+    # the calculation here is done from epoch == 0 instead of 1
+    def __init__(self, decay_after_epoch, power, max_epochs, optimizer, epoch):
         self.decay_after_epoch = decay_after_epoch
         self.max_epoch = max_epochs
         self.power = power
-        self.current_epoch = -1
-
-        self.last_epoch = -1
-        super(PolyLrDecay, self).__init__(optimizer, self.last_epoch)
+        if epoch == 1:
+            start_epoch = -1
+        else:
+            start_epoch = epoch
+        super(PolyLrDecay, self).__init__(optimizer, start_epoch)
 
     def get_lr(self):
         new_lrs = [
-            base_lr * (1 - self.current_epoch / self.max_epoch) ** self.power
+            base_lr * (1 - self.last_epoch / self.max_epoch) ** self.power
             for base_lr in self.base_lrs
         ]
 
         return new_lrs
 
     def step(self, epoch):
-        self.last_epoch = epoch
-        self.current_epoch = self.last_epoch
-
+        self.last_epoch = epoch - 1 if epoch != 0 else epoch
         if epoch % self.decay_after_epoch == 0:
             for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
                 param_group["lr"] = lr
